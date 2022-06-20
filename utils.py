5aefc5f2d23dc12f07e05e0a21ef900134e10c07
HaoZhu10015 <mrhaozhu@hotmail.com>
2022-06-20 22:58:01 +0200

    Add parameter illustrations.

--- utils.py
+++ utils.py
@@ -18,6 +18,22 @@ def column(arr):
 
 
 class TrajSimulator1D:
+    """
+    1-dimensional trajectory simulator.
+
+    Parameters
+    ----------
+    dt : float
+        Position sampling time interval.
+    duration : float
+        Total time length of the trajectory.
+    initial_state : numpy.array(n, 1)
+        Initial position (coordinate).
+    q_var : float
+        This is used to scale the state noise covariance matrix Q.
+    r_var : float
+        This is used to scale the observation noise covariance matrix R.
+    """
     def __init__(self, dt=0.01, duration=0.5, initial_state=column([0.0]), q_var=1000.0, r_var=10.0):
         self.dt = dt
         self.duration = duration
@@ -35,6 +51,16 @@ class TrajSimulator1D:
         self.R = np.array([[self.r_var]])
 
     def simulate(self):
+        """
+        Simulate a 1D trajectory.
+
+        Returns
+        -------
+        state: numpy.array()
+            Simulated trajectory (ground truth).
+        observation: numpy.array()
+            Observation of the simulated trajectory.
+        """
         # Simulate trajectory.
         current_state = self.initial_state
         state = []
@@ -54,6 +80,26 @@ class TrajSimulator1D:
 
 
 class KalmanFilterTuningModel:
+    """
+    Assembled Kalman filter model for auto-tuning process.
+
+    Parameters
+    ----------
+    dim_x : int
+        Number of state variables for the Kalman filter.
+    dim_z : int
+        Number of measurement inputs.
+    F : numpy.array()
+        State Transition matrix
+    H : numpy.array(dim_z, dim_x)
+        Measurement function.
+    initial_state :  numpy.array(dim_x, 1)
+        Initial state estimate.
+    P : numpy.array(dim_x, dim_x)
+        Current state covariance matrix.
+    observation : numpy.array(n, dim_z)
+        Observed trajectory.
+    """
     def __init__(
             self,
             dim_x, dim_z,
@@ -68,6 +114,21 @@ class KalmanFilterTuningModel:
         self.observation = observation
 
     def get_filter_nis_loss(self, Q, R):
+        """
+        Calculate NIS based object function for given Q and R.
+
+        Parameters
+        ----------
+        Q : numpy.array(dim_x, dim_x)
+            State noise covariance matrix.
+        R : numpy.array(dim_z, dim_z)
+            Observation noise covariance matrix.
+
+        Returns
+        -------
+        nis_loss: float
+            Calculated value of object function.
+        """
         self.kalman_filter.x = self.initial_state
         self.kalman_filter.P = self.P
         self.kalman_filter.Q = Q
